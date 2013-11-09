b37499ad53a5745958206c1601ec54d51f5500b8
AlexKtf <alexandre.ktifa@gmail.com>
2013-11-09 12:42:36 +0100

    Profile first try

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+  validates_uniqueness_of :username, :email
+
+  has_one :profile, :dependent => :destroy
+
+  
+  after_create :create_profile!
+end
---
+++ app/views/profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @profile, :first_name, :last_name, :user_id, :created_at, :updated_at
