68111797a6a18ac92b97be3cfbff59e8c8291df5
AlexKtf <alexandre.ktifa@gmail.com>
2013-11-10 16:52:29 +0100

    test for product creation

---
+++ spec/factories/products.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :product do
+    name 'Example'
+    description 'This is a great product'
+  end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Correctly create product' do
+    describe "#name_to_slug" do
+      let(:product) { FactoryGirl.create :product, name: 'mom & dad @home!' }
+
+      it "generate the right slug" do
+        product
+        product.slug.should == "mom_and_dad_at_home"
+      end
+    end
+  end
 end
