102e0cc667065f7d8ffae2c43b38fdfd5adbc6a9
AlexKtf <alexandre.ktifa@gmail.com>
2013-11-10 16:33:47 +0100

    fix focus

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
-describe User, focus: true do
+describe User do
   describe 'Correctly create user' do
     describe "#username_to_slug" do
       let(:user) { FactoryGirl.create :user, username: 'mom & dad @home!' }
+
       it "generate the right slug" do
         user
         user.slug.should == "mom_and_dad_at_home"
       end
     end
-
   end
 end
