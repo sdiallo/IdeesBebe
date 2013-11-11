775cf2c6b35d658723c2719ee766e03dcb3c0d80
AlexKtf <alexandre.ktifa@gmail.com>
2013-11-11 16:05:37 +0100

    Fix models method duplicate and path in layout

---
+++ app/models/concerns/slugable.rb
@@ -0,0 +1,30 @@
+module Slugable
+  extend ActiveSupport::Concern
+
+  def to_slug
+    #strip the string
+    if self.instance_of? Product
+      ret = self.name.strip
+    else 
+      ret = self.username.strip
+    end
+
+    #blow away apostrophes
+    ret.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with underscore
+     ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
+
+     #convert double underscores to single
+     ret.gsub! /_+/,"_"
+
+     #strip off leading/trailing underscore
+     ret.gsub! /\A[_\.]+|[_\.]+\z/,""
+
+     self.slug = ret
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,28 +13,8 @@ class User < ActiveRecord::Base
 
   
   after_create :create_profile!
-  before_save :username_to_slug
 
-  def username_to_slug
-    #strip the string
-    ret = self.username.strip
+  include Slugable
 
-    #blow away apostrophes
-    ret.gsub! /['`]/,""
-
-    # @ --> at, and & --> and
-    ret.gsub! /\s*@\s*/, " at "
-    ret.gsub! /\s*&\s*/, " and "
-
-    #replace all non alphanumeric, underscore or periods with underscore
-     ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '_'  
-
-     #convert double underscores to single
-     ret.gsub! /_+/,"_"
-
-     #strip off leading/trailing underscore
-     ret.gsub! /\A[_\.]+|[_\.]+\z/,""
-
-     self.slug = ret
-  end
+  before_save :to_slug, :if => :username_changed?
 end
