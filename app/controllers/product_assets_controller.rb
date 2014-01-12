000facacfc764b6de7f7099767daa5dce96ca6ff
AlexKtf <alexandre.ktifa@gmail.com>
2014-01-12 13:34:09 +0100

    Removed rename cancan var

--- app/controllers/product_assets_controller.rb
+++ app/controllers/product_assets_controller.rb
@@ -4,9 +4,6 @@ class ProductAssetsController < ApplicationController
   load_and_authorize_resource :product_asset, except: :create
   load_resource :product, only: :create
 
-
-  before_action :rename_cancan_var
-
   def create
     raise CanCan::AccessDenied if @product.user != current_user
     return redirect_to edit_product_path(@product.slug), alert: I18n.t('asset.file.presence') if not asset_params[:file].present?
@@ -21,29 +18,25 @@ class ProductAssetsController < ApplicationController
 
   # PUT /product_asset/1
   def update
-    if @asset.update(asset_params)
+    if @product_asset.update(asset_params)
       flash[:notice] = I18n.t('asset.become_starred.success')
     else
       flash[:alert] = I18n.t('asset.become_starred.error')
     end
-    redirect_to edit_product_path(@asset.product.slug)
+    redirect_to edit_product_path(@product_asset.product.slug)
   end
 
   # DELETE /product_asset/1
   def destroy
-    if @asset.destroy
+    if @product_asset.destroy
       flash[:notice] = I18n.t('asset.destroy.success')
     else
       flash[:alert] = I18n.t('asset.destroy.error')
     end
-    redirect_to edit_product_path(@asset.product.slug)
+    redirect_to edit_product_path(@product_asset.product.slug)
   end
 
   private
-    #TODO: Trouver une solution avec cancan
-    def rename_cancan_var
-      @asset = @product_asset
-    end
 
     def asset_params
       params.require(:product_asset).permit(:file, :starred)
