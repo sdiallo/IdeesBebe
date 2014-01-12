12450fb0714e9641c7808b9c15a0919a1ad70af0
AlexKtf <alexandre.ktifa@gmail.com>
2014-01-12 17:19:47 +0100

    Fix all product methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -22,8 +22,8 @@ class Category < ActiveRecord::Base
   before_save :to_slug, if: :name_changed?
 
   def all_products
-    return products unless main_category_id.nil?
-    Product.where('category_id IN (?)', subcategories.map(&:id)).order('created_at DESC')
+    return products unless subcategories.any?
+    products + Product.where('category_id IN (?)', subcategories.map(&:id)).order('created_at DESC')
   end
 
   class << self
