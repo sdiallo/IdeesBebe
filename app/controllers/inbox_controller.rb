5be0157bce4d20cfa271500e6ae3169e126ead26
AlexKtf <alexandre.ktifa@gmail.com>
2014-02-06 21:00:06 +0100

    debug inbound

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -5,10 +5,9 @@ class InboxController < ApplicationController
   def handle_inbound(event_payload)
     data = event_payload['msg']
 
-    slug, product_id = data['headers']['To'].chomp('@user.dev-ideesbebe.com').split('.')
-
+    slug, product_id = data['headers']['To'].split('@')[0]
     content = EmailReplyParser.parse_reply(data['text'])
-
+    raise "#{slug} , #{product_id}".inspect
     product = Product.find(product_id)
     sender = User.find_by_email(data['from_email'])
     receiver = User.find_by_slug(slug)
