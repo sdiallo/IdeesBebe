e9df801112ba69412766306e597c3ab7417734f4
AlexKtf <alexandre.ktifa@gmail.com>
2014-02-23 16:24:15 +0100

    Limit buyer to LIMIT_STRAIGtH messages and owner to one message straight

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,7 +3,7 @@ class Status < ActiveRecord::Base
   belongs_to :user
   has_many :messages
 
-  after_create :reactive_product, if: [:closed_changed?]
+  after_update :reactive_product, if: [:closed_changed?]
 
   def pending_messages_count
     date = messages.where(sender_id: product.user_id).maximum(:created_at) || DateTime.new
