d7fc19c8847d5d9ab703f1ff8d6992afde8a54ac
AlexKtf <alexandre.ktifa@gmail.com>
2014-03-31 20:26:20 +0200

    Add admin styles/Correctly redirect on admin connection

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,6 +5,12 @@ class Admin::SessionsController < Devise::SessionsController
     render layout: 'admin'
   end
 
+  def create
+    user = User.where('email = ? OR username = ?', params[:user][:login], params[:user][:login]).first
+    return redirect_to admin_path, alert: "Vous n'avez pas accés à cette partie du site" if user and not user.try(:is_admin)
+    super
+  end
+
   def after_sign_in_path_for resource
     admin_dashboard_index_path if request.path.starts_with?('/admin')    
   end
